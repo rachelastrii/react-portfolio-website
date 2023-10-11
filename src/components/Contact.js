b5356c113b819c87e3e00f567f1760f5fba332e7
rachelastrii <09021282025083@student.unsri.ac.id>
2023-10-11 21:53:31 +0700

    first website

---
+++ src/components/Contact.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import contactImg from "../assets/img/contact-img.svg";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Contact = () => {
+
+  return (
+    <section className="contact" id="connect">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} md={6}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
+              }
+            </TrackVisibility>
+          </Col>
+          <Col size={12} md={6}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <h2>Interest</h2>
+              
+                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
---
+++ src/components/Newsletter.js
@@ -0,0 +1,12 @@
+import { Col } from "react-bootstrap";
+
+export const Newsletter = () => {
+  return (
+      <Col lg={12}>
+        <div className="newsletter-bx wow slideInUp" id="awards">
+              <h3>Awards</h3>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+        </div>
+      </Col>
+  )
+}
